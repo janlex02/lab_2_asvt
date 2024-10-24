bc56f1026591b1b5fd98e0187374956287991512
janlex02 <83964902+janlex02@users.noreply.github.com>
2024-10-24 17:27:18 +0300

    Add files via upload

---
+++ asvt/asvt_laba_2/proga_asvt_laba_2.cs
@@ -0,0 +1,49 @@
+﻿/////////////////////////////////
+//      README.md first!       //
+//                             //
+//      Лаба по АСВТ №2        //
+//          Потоки             //
+//     Горбачев Матвей 4305    //
+/////////////////////////////////
+using System;
+using System.Threading;
+
+class proga_asvt
+{
+    static long Factorial(int n)
+    {
+        long result = 1;
+        if(n==0) return result;
+        for(int i = 1; i<n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static void CalculatingFactorial(object number)
+    {
+        int num = (int)number;
+        long result = Factorial(num);
+        Console.WriteLine($"Факториал {num} = {result}");
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = {2, 4, 6, 20, 50};
+        Thread[] threads = new Thread[numbers.Length];
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            threads[i] = new Thread(CalculatingFactorial);
+            threads[i].Start(numbers[i]);
+        }
+
+        for(int i = 0; i < threads.Length; i++)
+        {
+            threads[i].Join();
+        }
+        Console.WriteLine("Работа программы завершена ставьте 5 за семестр(пожалуйста)");
+    }
+
+}
